651976af774bdf5e918a06b2410eb4b2e502cfd8
Sajid305 <mdshajid305@gmail.com>
2018-10-22 10:55:14 +0600

    Exercise

---
+++ Source code/exercise7.py
@@ -0,0 +1,19 @@
+# exercise
+# define a function that takes a number(n)
+# return a dictionary containing cube of numbers from 1 to n
+ 
+# example 
+# cube_finder(3)
+# {1:1, 2:8, 3:27}
+
+#Solution
+
+def number(n):
+    cubes = {}
+    for i in range(1,n+1):
+        cubes[i] = i**3
+    return cubes
+
+print(number(11))
+
+
