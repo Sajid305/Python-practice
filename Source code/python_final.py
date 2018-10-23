23f3fcfe5340ef95e33621f58bfe64629d7a0768
Sajid305 <mdshajid305@gmail.com>
2018-10-23 22:48:38 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -902,3 +902,30 @@
 
 # for student_var in students:
 #     print(student_var)
+
+
+                                            # exercise
+# define a function that takes a number(n)
+# return a dictionary containing cube of numbers from 1 to n
+ 
+# example 
+# cube_finder(3)
+# {1:1, 2:8, 3:27}
+
+#Solution
+
+# def number(n):
+#     cubes = {}
+#     for i in range(1,n+1):
+#         cubes[i] = i**3
+#     return cubes
+
+# print(number(11))
+
+
+# def finder(n):
+#     cubs={}
+#     for i in range(1,n+1):
+#         cubs[i]=i**3
+#     return cubs
+# print(finder(20))
