ee6b629794a77416867ad66fe94d8acec9ff892b
Sajid305 <mdshajid305@gmail.com>
2018-10-23 22:52:35 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -938,3 +938,18 @@
 # print(data)
 # data = dict.fromkeys(range(1,12),"unknown")               #dict useing range function
 # print(data)
+
+
+                                                # get method
+
+# d = {'name':'shajid','roll':'1512','age':'23'}
+# print(d)
+# print(d['name'])
+# print(d.get("name"))
+# if d.get('name'):
+#     print("YEs")
+# else:
+#     print("NO")                                   
+
+# d = {'name':'shajid','name':'rayhan','roll':'1512','age':'23','country':'Bangladesh','City':'Chittagong'}
+# print(d.get('name'))              # get method ovarwrite thos same key, 
