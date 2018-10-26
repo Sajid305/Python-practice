141e2845702729a4cc2c80c973786b7622a321d7
Sajid305 <mdshajid305@gmail.com>
2018-10-26 16:21:02 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -981,3 +981,21 @@
 #         counted[car] = s.count(car)
 #     return counted
 # print(count('shajid'))
+
+
+
+                                    # Store data from user
+# user = {}
+
+# name = input('enter your name : ')
+# age = input('enter your age : ')
+# fev_movies = input('enter your fev movies separated by comma : ').split(',')
+# fev_song = input('enter your fev song separated by comma : ').split(',')
+
+# user['name'] = name
+# user['age'] = age
+# user['fev_movies'] = fev_movies
+# user['fev_song'] = fev_song
+
+# for key,value in user.items():
+#     print(f'{key} : {value}')
