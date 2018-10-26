d068b8e8a80f7fa3c04e4d79a171f88a0f4618a0
Sajid305 <mdshajid305@gmail.com>
2018-10-26 17:56:13 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -1080,7 +1080,7 @@
 # print(d)
 
                                                     # set data type
-                                                    
+
 # unordered collection of unique items 
  
 # s = {1,2,3}
@@ -1100,3 +1100,40 @@
 # s1 = s.copy()
 # print(s1)
 # print(s)
+
+
+
+                                              #  Set practice
+                                              
+# s= {1,1,2,3,4,5,56,3,2,1,22,3,3,1,5,5,5}
+# print(s)
+
+# list_ = [1,2,3,4,5,5,77,8,98,9,2,1,23,3,7,8,90,8,6,9]
+# covert list into set
+
+# s = set(list_)
+# print(s)
+
+# then again convert set into list
+
+# list_ = [1,2,3,4,5,5,77,8,98,9,2,1,23,3,7,8,90,8,6,9]
+# s = list(set(list_))
+# print(list_)
+# print(s)
+
+# using in method in set
+
+# s = {1,2,3,4,5,5,77,8,98,9,2,1,23,3,7,8,90,8,6,9}
+# print(s)
+# if 1 in s:
+#     print(True)
+# else:
+#     print(False)
+
+
+# looping in set
+# s = {1,2,3,4,5,5,77,8,98,9,2,1,23,3,7,8,90,8,6,9} 
+# for i in s:
+#     print(i)
+
+
