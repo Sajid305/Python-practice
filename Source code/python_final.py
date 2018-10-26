459309f459961d1a96d714dab649590a16aba18a
Sajid305 <mdshajid305@gmail.com>
2018-10-26 17:54:28 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -1001,7 +1001,7 @@
 #     print(f'{key} : {value}')
 
 
-                                                                                    # summary dictionary
+                                                  # summary dictionary
 # what is dictionary
 # unordered collection of data 
  
@@ -1078,3 +1078,25 @@
 # popped = d.popitem()
 # print(popped)
 # print(d)
+
+                                                    # set data type
+                                                    
+# unordered collection of unique items 
+ 
+# s = {1,2,3}
+ 
+# s = {1,1.1, 2.3, 'string'}
+ 
+# print(s[1])
+# l = [1,2,3,4,5,5,5,5,6,7,7,8]
+# remove duplicate 
+# s2 = list(set(l))
+# print(s2)
+# s.add(4)
+# s.add(5)
+# s.add(4)
+# s.remove(3)
+# s.discard(5)
+# s1 = s.copy()
+# print(s1)
+# print(s)
