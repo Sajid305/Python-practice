866e8484860cd4bd916f0693600659060ba28720
Sajid305 <mdshajid305@gmail.com>
2018-10-27 15:31:01 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -1213,3 +1213,41 @@
 # output = [i*2 if (i%2 == 0) else(-i) for i in number]
 # print(output)
                     
+                    # list comprehension practice
+
+# data = ['rayhan','shajid','bangladesh']
+
+# reverseing word useing list comprehension
+
+# def revers_list(L):
+#     return[rev_word[::-1] for rev_word in L]
+# print(revers_list(data))
+
+
+# reverseing word normal way
+
+# def revers_list(l):
+#     rev_car = []
+#     for rev_word in l:
+#         rev_car.append(rev_word[::-1])
+#     return rev_car
+# print(revers_list(data))
+
+
+#                                            nested list comprehension
+
+
+#jenarate a list like this  list_ = [ [1,2,3],[4,5,6],[7,8,9] ] 
+
+# with list comprehension
+ 
+# j_list = [[i for i in range(1,4)] for j in range(3)]
+# print(j_list)
+
+
+# Without list comprehension
+
+# j_list = []
+# for i in range(3):
+#     j_list.append([1,2,3])
+# print(j_list)
