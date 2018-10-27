f6dcc483d3ee3c7f31a0d26d5d992a1f0de999fd
Sajid305 <mdshajid305@gmail.com>
2018-10-27 18:11:02 +0600

    Dictionary Comprehension practice

---
+++ Source code/dict_comprehension/dict_comprehension.py
@@ -0,0 +1,8 @@
+
+                            # Dictionary Comprehension
+
+# square of Dictionary Value With dictionary comprehension
+
+# dict_ = {f"square of {num} is":num*2 for num in range(1,11)}
+# for k,v in dict_.items():
+#     print(f"{k} : {v}")
