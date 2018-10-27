9e322ef8cfdd3f54dfe2cc23c85b4f92ff313683
Sajid305 <mdshajid305@gmail.com>
2018-10-27 14:48:02 +0600

    list comprehension practice

---
+++ Source code/list_comprehension_practice.py
@@ -0,0 +1,16 @@
+data = ['rayhan','shajid','bangladesh']
+# reverseing word useing list comprehension
+
+# def revers_list(L):
+#     return[rev_word[::-1] for rev_word in L]
+# print(revers_list(data))
+
+
+# reverseing word normal way
+
+# def revers_list(l):
+#     rev_car = []
+#     for rev_word in l:
+#         rev_car.append(rev_word[::-1])
+#     return rev_car
+# print(revers_list(data))
