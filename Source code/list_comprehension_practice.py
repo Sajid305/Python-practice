e65f0f8304eeb49f97346c744eadcdeeeccdda2f
Sajid305 <mdshajid305@gmail.com>
2018-10-27 15:18:58 +0600

    list comprehension practice

--- Source code/list_comprehension_practice.py
+++ Source code/list_comprehension_practice.py
@@ -1,4 +1,5 @@
-data = ['rayhan','shajid','bangladesh']
+# data = ['rayhan','shajid','bangladesh']
+
 # reverseing word useing list comprehension
 
 # def revers_list(L):
@@ -14,3 +15,24 @@ data = ['rayhan','shajid','bangladesh']
 #         rev_car.append(rev_word[::-1])
 #     return rev_car
 # print(revers_list(data))
+
+
+#                                            nested list comprehension
+
+
+#jenarate a list like this  list_ = [ [1,2,3],[4,5,6],[7,8,9] ] 
+
+# with list comprehension
+ 
+# j_list = [[i for i in range(1,4)] for j in range(3)]
+# print(j_list)
+
+
+# With out list comprehension
+
+j_list = []
+for i in range(3):
+    j_list.append([1,2,3])
+print(j_list)
+
+
