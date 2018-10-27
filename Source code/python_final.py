0cec5475b86e8955d3703fcf6f886f619c5e70a9
Sajid305 <mdshajid305@gmail.com>
2018-10-27 14:42:38 +0600

    updated

--- Source code/python_final.py
+++ Source code/python_final.py
@@ -1177,3 +1177,39 @@
 # name = ['MD','Shajid','Rayhan']
 # name2 = [i[0] for i in name]
 # print(name2)
+
+                                       # list comprehension exercise
+
+# num to string from a list
+# [True, False , [1,2,3], 1, 1.0, 3]
+
+# list_=[False,True, [1,2,3,4,5,6], [1.3,1.5,1.67]]
+
+# def num_to_string(l):
+#     return[str(i) for i in l if(type(i) == int or type(i) == float or type(i) == list)]
+
+# print(num_to_string(list_))
+
+
+
+                                # list comprehension exercise
+
+# useing if else3 without list comprihension
+
+# number = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# output = []
+# for i in number:
+#     if i%2 == 0:
+#         output.append(i*2)
+#     else:
+#         output.append(-i)
+# print(output)
+
+
+# useing if else with list comprehension
+
+# number = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+# output = [i*2 if (i%2 == 0) else(-i) for i in number]
+# print(output)
+                    
