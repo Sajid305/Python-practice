7e0b277e55663ed66b51ba8315dee73b4b1b467e
Sajid305 <mdshajid305@gmail.com>
2018-11-02 16:46:45 +0600

    args with normal parameter

---
+++ Source code/star args oparator and flexible functions/args with normal parameter.py
@@ -0,0 +1,9 @@
+
+# * agrs with normal parameter
+
+# def multiply_nums(num,*args):
+#     multiply = 1
+#     for i in args:
+#         multiply *= i
+#     return multiply
+# print(multiply_nums('a',1,2,3,4))
