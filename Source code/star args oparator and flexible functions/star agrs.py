eb67021fa09f4287c19e5e4fc59ce6e74ff56734
Sajid305 <mdshajid305@gmail.com>
2018-11-02 16:28:01 +0600

    star args practice

---
+++ Source code/star args oparator and flexible functions/star agrs.py
@@ -0,0 +1,19 @@
+                # flexible function
+            # * oparators
+        # *args
+
+# def total(a,b):
+#     return a+b            # without *args
+# print(total(1,2))
+
+# def total(a,b):
+#     return a+b                # without *args
+# print(total(1,2,3))
+
+# def total(*args):
+#     number = 0
+#     for i in args:            # with *args
+#         number += i
+#     return number
+# print(total(1,2,3))
+
