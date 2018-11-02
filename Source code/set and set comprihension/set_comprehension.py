b14de5c1ccc8b647436ac4f0ad33a553c9b981b4
Sajid305 <mdshajid305@gmail.com>
2018-11-02 16:03:39 +0600

    set comprehension

---
+++ Source code/set and set comprihension/set_comprehension.py
@@ -0,0 +1,9 @@
+
+                                            # set comprehension
+
+# set_ = {k**2 for k in range(1,10)}
+# print(set_)
+
+# names = ['shajid','rayhan','ahad']            #set is unorder
+# first = {name[0] for name in names}
+# print(first)
