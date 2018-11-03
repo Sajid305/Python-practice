0456a72785469f6d501b45b7e28728b0937624d7
Sajid305 <mdshajid305@gmail.com>
2018-11-04 01:34:18 +0600

    Lambda Expression Practice

---
+++ Source code/Lambda Expression/Lambda Expression Practice .py
@@ -0,0 +1,49 @@
+
+
+                                    # Lambda Expression Practice 
+
+# def is_even(i):
+#     if i%2 == 0:
+#         return True
+#     else:
+#         return False
+# print(is_even(5))
+
+# def is_even(s):
+#     return s%2 == 0  # s%2 == 0 ----> true , false
+# print(is_even(4))
+
+# def return_last_carecter(s):
+#     return s[-1]
+# print(return_last_carecter("shajid"))
+ 
+                                    # same function with lambda expression
+
+# is_even = lambda s : s%2 == 0
+# print(is_even(9))
+
+# return_last_carecter = lambda s : s[-1]
+# print(return_last_carecter("shajid"))
+
+                                        # lambda with if else
+
+# def function(s):
+#     if len(s) > 5:
+#         return True
+#     else:
+#         return False
+
+# print(function("sha"))    
+
+# def function(s):
+#     return len(s) > 5
+# print(function("shajid"))
+
+                                # Same function with if else
+
+# function = lambda s : True if len(s)>5 else False
+# print(function("sha"))
+
+# function = lambda s : len(s)>5
+# print(function("shajid"))
+
