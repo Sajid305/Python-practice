c7717c2c4f93deaaa7f99a1f9315b72cffa3de84
Sajid305 <mdshajid305@gmail.com>
2018-11-03 17:48:12 +0600

    Exercise 1 solution

---
+++ Source code/star args oparator and flexible functions/Exercise 1 solution.py
@@ -0,0 +1,11 @@
+
+
+                                # Exercise 1 solutin
+
+def to_power(num,*args):
+    if args:
+        return [i**num for i in args]
+    else:
+        return "you didn't pass any args"
+nums = [1,2,3]
+print(to_power(2,*nums))
