21f85301b837874a268998675ce2c8ea0fc4bbd8
Sajid305 <mdshajid305@gmail.com>
2018-11-04 01:00:45 +0600

    Lambda Expression

---
+++ Source code/Lambda Expression/Lambda Expression.py
@@ -0,0 +1,13 @@
+            
+                                            # Lambda Expression
+# def add(a,b):
+#     return a+b
+# print(add(3,4))
+
+# ab = lambda a,b : a+b
+# print(ab(1,2))
+
+# we use lambda with some built in function like map,filter etc;
+
+# multiply = lambda a,b : a*b
+# print(multiply(2,3))
