7d4937bd8b595444647185d9361dadae12b579cb
Sajid305 <mdshajid305@gmail.com>
2018-11-06 01:35:47 +0600

    iterator vs iterables

---
+++ Source code/so many kind of function , like map,Enumerate,filter etc/iterator vs iterables.py
@@ -0,0 +1,9 @@
+                                
+                                
+                                    # iterator vs iterables 
+
+
+# numbers = [1,2,3,4] # , tuples, string --- iterables 
+# squares = map(lambda a:a**2, numbers) # iterator 
+ 
+# print(next(numbers))
