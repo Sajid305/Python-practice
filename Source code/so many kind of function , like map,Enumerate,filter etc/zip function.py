9bc4005670c830a9e74ffd30f36bec393abfe187
Sajid305 <mdshajid305@gmail.com>
2018-11-06 01:55:03 +0600

    ZIP Function

---
+++ Source code/so many kind of function , like map,Enumerate,filter etc/zip function.py
@@ -0,0 +1,17 @@
+
+                                            
+                                            # ZIP Function
+
+# userId = ['user1','user2','user3']
+# userName = ['shajid','rayhan','ahad']
+
+# ziped_data = list(zip(userId,userName))   # we can use multiple class like /list /dictionary 
+#  ziped_data = dict(zip(userId,userName))
+# print(type(userId))
+
+                                    
+                                    #     Example
+
+# example = [('a',1),('b',2)]               # this is how we can change tuple in side of list in to dictionary
+# print(dict(example))
+
