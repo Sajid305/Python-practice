7c51ed65bef438a9e7c8d0c32c872d1c81da5cc8
Sajid305 <mdshajid305@gmail.com>
2018-11-09 03:34:17 +0600

    Geneator Comprehension

---
+++ Source code/Generators/Generator Comprehension .py
@@ -0,0 +1,10 @@
+
+                                        
+                                        # Generator Comprehension 
+
+# Generator Comprehension is like list comprehension it just use first brackets insted of [] brackets
+
+square = (i**2 for i in range(1,11))
+
+for num in square:
+    print(num)
