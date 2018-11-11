233a991fae0b69d7ba640fffd69f70413c0c3bc1
Sajid305 <mdshajid305@gmail.com>
2018-11-11 23:16:34 +0600

    OOP Class Method as a Constructor

---
+++ Source code/Object Oriented Programming/Class Method as a constructor.py
@@ -0,0 +1,22 @@
+
+                                    
+                                    
+                                            # OOP Class Method as a Constructor 
+
+
+# in this programm we going to make our one constructor 
+
+
+class Person:
+    count_object = 0
+    def __init__(self,first_name,last_name,age):
+        Person.count_object +=1
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @classmethod
+    def counted(cls):
+        return f'you have created {cls.count_object}'
+
+  
