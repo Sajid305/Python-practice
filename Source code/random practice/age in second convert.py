bb6b9307e68c6578aa2e634698684289ff79b61b
Sajid305 <mdshajid305@gmail.com>
2018-11-17 21:00:52 +0600

    age in second convert programm

---
+++ Source code/random practice/age in second convert.py
@@ -0,0 +1,20 @@
+
+                                                        know how many seconds you have lived in your age #
+
+print("enter your age if you want to see your age in seconds")
+age = input("enter your age: ")
+age_in_seconds = int(age)*360*24*60*60
+print('you lived for {} seconds in your age'.format(age_in_seconds))
+
+age = int(input("enter your age: "))
+def seconds_in_life(int_):
+    life_in_seconds = int_*360*24*60*60                   # functional way....
+    return life_in_seconds
+ans = seconds_in_life(age)
+print(f"You have lived {ans} seconds in your life....get a girl hehehe")
+
+def age_in_seconds():
+    user_age = int(input("enter your age: "))
+    age_seconds = user_age*365*24*60*60                                          # more simplifying way
+    print(age_seconds)
+age_in_seconds()
