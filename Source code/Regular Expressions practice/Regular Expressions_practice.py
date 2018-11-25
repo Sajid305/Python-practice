d04584902631166aab90aaae210e552169710784
Sajid305 <mdshajid305@gmail.com>
2018-11-25 22:11:05 +0600

    Regular Expressions practice

---
+++ Source code/Regular Expressions practice/Regular Expressions_practice.py
@@ -0,0 +1,52 @@
+
+
+                                         # Regular Expressions practice
+
+
+# import re
+# count = 0
+# pattern = re.compile('shajid')
+# matcher = pattern.finditer('shajid Rayhan sajid sajid')
+# for match in matcher:
+#     count += 1
+#     print('the number is available at start index',match.start())
+# print("the number is available at start index : ",count)
+
+# import re
+# pattern = re.compile('ab')
+# matcher = pattern.finditer('abbblabblablab')
+# count = 0
+# for match in matcher:
+#     count +=1
+#     print(f"start is {match.start()} end is {match.end()} group is {match.group()}")
+
+# print(f'the number is available at start index : {count} ')
+
+# import re
+
+# matcher = re.finditer('ab','abbblabblablab')
+# count = 0
+# print(type(matcher))
+# for match in matcher:
+#     count +=1
+#     print(f"start is {match.start()} end is {match.end()} group is {match.group()}")
+
+# print(f'the number is available at start index : {count} ')
+
+
+# import re
+
+# matcher = re.finditer('[^a-zA-Z0-9]','a8fdgdfas8232jk@5442345iojnfasdu9')
+# count = 0
+# print(type(matcher))
+# for match in matcher:
+#     count +=1
+#     print(f" {match.start()}------{match.group()}")
+  
+
+# print(f'the number is available at start index : {count} ')
+
+                    
+                    
+                                    
+                                    
