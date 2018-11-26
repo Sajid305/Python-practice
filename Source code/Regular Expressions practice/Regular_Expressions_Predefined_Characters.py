c45c1a08f874ac1713e402cf9f00a561908752f3
Sajid305 <mdshajid305@gmail.com>
2018-11-26 21:46:35 +0600

    Predefind Character Classes

--- Source code/Regular Expressions practice/Regular_Expressions_Predefined_Characters.py
+++ Source code/Regular Expressions practice/Regular_Expressions_Predefined_Characters.py
@@ -1,13 +1,66 @@
 
+
+
+                               # :::Predefined Character Classes:::
+
+
 import re
 
-# matcher = re.finditer('\s','ab4cd7 @*&5oifd4321')
+
+
+# matcher = re.finditer('\s','ab4cd7 @*&5oifd4321') #   <----------space character
 
 # for match in matcher:
 #     print(f'{match.start()}-----{match.group()}')
 
 
-matcher = re.finditer('\W','b4cd7 @*&5oifd4321')
 
-for match in matcher:
-    print(f'{match.start()}-----{match.group()}')
+
+# <--------------\S-------------> Except space character
+
+
+# matcher = re.finditer('\S','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
+
+
+#<----------------\d-------------> [0-9] any digit
+
+
+# matcher = re.finditer('\d','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
+
+
+#<----------------\D-------------> Except digit
+
+
+# matcher = re.finditer('\D','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
+
+#<----------------\w-------------> Any word character(alpha numerick)
+
+# matcher = re.finditer('\w','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
+
+
+#<----------------\W-------------> Any character Except word(specialchar) 
+
+# matcher = re.finditer('\W','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
+
+#<----------------([ . ])-------------> Every character 
+
+
+# matcher = re.finditer('.','b4cd7 @*&5oifd4321')
+
+# for match in matcher:
+#     print(f'{match.start()}-----{match.group()}')
