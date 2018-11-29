485d823a330602b5218792a540db8561be7d756f
Sajid305 <mdshajid305@gmail.com>
2018-11-29 19:15:34 +0600

    Read Text Files

---
+++ Source code/Object Oriented Programming/Read Text Files.py
@@ -0,0 +1,33 @@
+
+# read file
+# open function 
+# read method
+# tell method
+# readline method
+# readline methods
+# close method
+
+f = open('filel.txt')
+print(f'cursor position -- {f.tell()}')
+
+# print(f.read())
+# print(f.readline(), end='')
+# print(f.readline())
+# print(f'cursor position -- {f.tell()}')
+# print('befor seek method')
+# f.seek(0)
+# print('after seek method')
+# print(f'cursor position -- {f.tell()}')
+# print(f.read())
+
+# f = open('flle.text')
+# print(len(lines))
+# for line in lines:
+#     print(line.end = '')
+
+f = open("d:\new_folder\file.txt")
+# windows -\
+# linux , mac /
+# name,closed
+print(f.close())
+f.close()
